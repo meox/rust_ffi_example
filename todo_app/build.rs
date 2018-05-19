936e43420993b1d84b0599291efa2d7557c02ce9
Gian Lorenzo Meocci <glmeocci@gmail.com>
2018-05-19 17:21:59 +0200

    fix

--- todo_app/build.rs
+++ todo_app/build.rs
@@ -1,5 +1,5 @@
  
 fn main() {
     println!("cargo:rustc-link-lib=dylib=todolist");
-    println!("cargo:rustc-link-search=native=/home/meox/Dropbox/rustc/todolist/");
+    println!("cargo:rustc-link-search=native=../todolist");
 }
